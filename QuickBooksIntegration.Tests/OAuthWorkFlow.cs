3334e17f8b185f94749663d9001fba3c45fb4e64
Matt Davis <matt.davis@qsxtechnology.co.uk>
2017-10-25 20:23:09 -0700

    Added a class to encapsulate the auth flow

--- QuickBooksIntegration.Tests/OAuthWorkFlow.cs
+++ QuickBooksIntegration.Tests/OAuthWorkFlow.cs
@@ -5,57 +5,97 @@ using System.Net.Http;
 using System.Threading.Tasks;
 using FluentAssertions;
 using System.Net.Http.Headers;
+using Newtonsoft.Json;
 
 namespace QuickBooksIntegration.Tests
 {
-  class OAuthWorkFlow
+  class IntuitAuthClient
   {
-    [Test]
-    public async Task GetAccessToken()
+    private readonly HttpClient client;
+
+    public IntuitAuthClient(string clientId, string sharedSecret)
+    {
+      client = new HttpClient() { BaseAddress = new Uri("https://oauth.platform.intuit.com") };
+      client.DefaultRequestHeaders.Authorization =
+        new AuthenticationHeaderValue("Basic", Base64EncodeClientSecret(clientId, sharedSecret));
+      client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+    }
+
+    private static string Base64EncodeClientSecret(string clientId, string sharedSecret)
+    {
+      return Convert.ToBase64String(
+          System.Text.Encoding.UTF8.GetBytes(
+              $"{clientId}:{sharedSecret}"));
+    }
+
+    public async Task<Token> GetAccessToken(string accessCode)
     {
       var data = new Dictionary<string, string>
       {
-        {"code", "Q011508832773ZtQ3qa4ShTskMtYRmYNVhlnhZ00Aj8TANtWEn" },
+        {"code", accessCode },
         {"grant_type", "authorization_code" },
         {"redirect_uri", "https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl" }
       };
-      var client = new HttpClient() { BaseAddress = new Uri("https://oauth.platform.intuit.com") };
-      client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Basic", "UTA1SHB4a0QzRG8yb0dMMjU0VUdNdWlZQXprVk1HaXpudDhHSXhMUlc1bll5Vnp2eDQ6WlM2cE8yamc1QTN4RHpDcEZRTG4zMkZWdWQ2UlZhOW1kdE9MQlhZRQ==");
-      client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
       var getAccessTokenResponse = await client.PostAsync("oauth2/v1/tokens/bearer", new FormUrlEncodedContent(data));
 
-      var jwtToken = await getAccessTokenResponse.Content.ReadAsAsync<dynamic>();
+      return await getAccessTokenResponse.Content.ReadAsAsync<Token>();
+    }
 
-      System.Console.WriteLine(jwtToken.access_token);
+    public async Task<Token> RefreshAccessToken(Token token)
+    {
+      var data = new Dictionary<string, string>
+      {
+        {"grant_type", "refresh_token" },
+        {"refresh_token", token.RefreshToken }
+      };
 
-      ((string)jwtToken.refresh_token).Should().NotBeEmpty();
+      var getAccessTokenWithRefreshResponse = await client.PostAsync("oauth2/v1/tokens/bearer", new FormUrlEncodedContent(data));
+
+      return await getAccessTokenWithRefreshResponse.Content.ReadAsAsync<Token>();
+    }
+  }
+
+  class Token
+  {
+    [JsonProperty("access_token")]
+    public string AccessToken { get; set; }
+
+    [JsonProperty("refresh_token")]
+    public string RefreshToken { get; set; }
+  }
+
+  class OAuthWorkFlow
+  {
+    private string AccessCode = "Q011508988380EWqY6Jbq38YyDNEfnseZ7HGPg7CPyy69XvBQp";
+
+    private const string ClientId = "Q05HpxkD3Do2oGL254UGMuiYAzkVMGiznt8GIxLRW5nYyVzvx4";
+    private const string ClientSecret = "ZS6pO2jg5A3xDzCpFQLn32FVud6RVa9mdtOLBXYE";
+
+    private IntuitAuthClient authClient;
+    private Token token;
+    private HttpClient resourceClient;
+
+    [OneTimeSetUp]
+    public async Task SetUp()
+    {
+      resourceClient = new HttpClient() { BaseAddress = new Uri("https://sandbox-quickbooks.api.intuit.com") };
+      resourceClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+      authClient = new IntuitAuthClient(ClientId, ClientSecret);
+
+      token = await authClient.GetAccessToken(AccessCode);
+
+      Console.WriteLine(token.AccessToken);
+      Console.WriteLine(token.RefreshToken);
     }
 
     [Test]
     public async Task GetResourceWithAccessToken()
     {
-      var data = new Dictionary<string, string>
-      {
-        {"code", "Q011508835392OyPsflbjNvlths5DWxaODZNZIXqFdMIHm0hnl" },
-        {"grant_type", "authorization_code" },
-        {"redirect_uri", "https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl" }
-      };
-      var authClient = new HttpClient() { BaseAddress = new Uri("https://oauth.platform.intuit.com") };
-      authClient.DefaultRequestHeaders.Authorization =
-        new AuthenticationHeaderValue("Basic", "UTA1SHB4a0QzRG8yb0dMMjU0VUdNdWlZQXprVk1HaXpudDhHSXhMUlc1bll5Vnp2eDQ6WlM2cE8yamc1QTN4RHpDcEZRTG4zMkZWdWQ2UlZhOW1kdE9MQlhZRQ==");
-      authClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-      var getAccessTokenResponse = await authClient.PostAsync("oauth2/v1/tokens/bearer", new FormUrlEncodedContent(data));
-
-      var jwtToken = await getAccessTokenResponse.Content.ReadAsAsync<dynamic>();
-
-      string accessToken = jwtToken.access_token;
-
-      //string accessToken = "eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..lddXflRKr3eklU3oPJMSxg._eN05xc5zzSjFVqNl_0-t2UUL6mkpBwH0x1Lw4KH1PeJ4TZ3mHdAXMh7J2HPm1aLTFb3WkOIeX6xmf3-Fgnk5pF4mbjltDQ-9M6zuC0Bncmy4b5Y40IRUZnEdUKIxfz1MMC6tiwJBaArWza_Bln_drwu9zlRwGgyLzKDLUm9WR9v_Zc_MFFomTzKA6iU7NafcaE64qxWeIp6moPfkB6fYjeRMgjZqldE7ALt0DB8gNWXfrd3m-n5pObO5pkvfmIEIadxKcqegUW_Y3eiIG1h6xLSq6IVSI_MvXH_APfV7JdJ9PT-DUgI5N0fxuAfE67rJhL7eP9nVI6oqDAozjhD5RM0l3CBmpkLH-zU7MUfhug0WGwlj3zrAdRxDWbMvthQwug0KLrn5jtNsCRNK-ugteAnMypA1Zl-uJigmMtVgyN-BLrX_Cij04hmH8vZ9G2Uoo4OBPNEeuodauR5ZZG0wDbYLgEgAY9xgRemmIo6FwY_tK042PzZgE2AZEb8ERgZjLLTCjClWm-W6ellyaJBuVncCRCkR6RyvRHLZsL1beF8fqAJkKz9zFb51cz8D-yvZN-myGiIOFDGElIua_YgDsfwUxDnZaunf3VH0QiwuNhqB4qD3_m7hiSWhX4nHUQxPkBimzlKtS0yJn0LiQp2OZpFumrGEZOunKfGJ8oh_9UNlpUksH7yDf-HBcfJHsWj3JVXy8u5tS-pp7o3nB_SWVRMSWLnI1JhaqgVW8SjKUc.QeetnOpKxmx1mjLtAmW4UQ";
-      var resourceClient = new HttpClient() { BaseAddress = new Uri("https://sandbox-quickbooks.api.intuit.com") };
       resourceClient.DefaultRequestHeaders.Authorization =
-        new AuthenticationHeaderValue("Bearer", accessToken);
-      resourceClient.DefaultRequestHeaders.Accept.Clear();
-      resourceClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        new AuthenticationHeaderValue("Bearer", token.AccessToken);
+
       var getResourceResponse = await resourceClient.GetAsync("v3/company/193514651873324/companyinfo/193514651873324");
 
       Console.WriteLine(await getResourceResponse.Content.ReadAsStringAsync());
@@ -65,46 +105,16 @@ namespace QuickBooksIntegration.Tests
       ((string)company.CompanyInfo.CompanyName).Should().Be("Sandbox Company_US_1");
     }
 
-
     [Test]
     public async Task RefreashAccessTokenAndGetResource()
     {
-      var data = new Dictionary<string, string>
-      {
-        {"code", "Q011508911870v6HbgJEoTFuua9qnYx8RWePaKAFMA7KxTCXbs" },
-        {"grant_type", "authorization_code" },
-        {"redirect_uri", "https://developer.intuit.com/v2/OAuth2Playground/RedirectUrl" }
-      };
-      var authClient = new HttpClient() { BaseAddress = new Uri("https://oauth.platform.intuit.com") };
-      authClient.DefaultRequestHeaders.Authorization =
-        new AuthenticationHeaderValue("Basic", "UTA1SHB4a0QzRG8yb0dMMjU0VUdNdWlZQXprVk1HaXpudDhHSXhMUlc1bll5Vnp2eDQ6WlM2cE8yamc1QTN4RHpDcEZRTG4zMkZWdWQ2UlZhOW1kdE9MQlhZRQ==");
-      authClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
-      var getAccessTokenResponse = await authClient.PostAsync("oauth2/v1/tokens/bearer", new FormUrlEncodedContent(data));
+      token = await authClient.RefreshAccessToken(token);
 
-      var jwtToken = await getAccessTokenResponse.Content.ReadAsAsync<dynamic>();
+      Console.WriteLine($"Access token from refresh token\n{token.AccessToken}");
 
-      Console.WriteLine($"Access token from access code\n{jwtToken}");
-
-      data = new Dictionary<string, string>
-      {
-        {"grant_type", "refresh_token" },
-        {"refresh_token", (string)jwtToken.refresh_token }
-      };
-
-      var getAccessTokenWithRefreshResponse = await authClient.PostAsync("oauth2/v1/tokens/bearer", new FormUrlEncodedContent(data));
-
-      var jwtTokenFromRefresh = await getAccessTokenWithRefreshResponse.Content.ReadAsAsync<dynamic>();
-
-      Console.WriteLine($"Access token from refresh token\n{jwtTokenFromRefresh}");
-
-      string accessToken = jwtTokenFromRefresh.access_token;
-
-      //string accessToken = "eyJlbmMiOiJBMTI4Q0JDLUhTMjU2IiwiYWxnIjoiZGlyIn0..lddXflRKr3eklU3oPJMSxg._eN05xc5zzSjFVqNl_0-t2UUL6mkpBwH0x1Lw4KH1PeJ4TZ3mHdAXMh7J2HPm1aLTFb3WkOIeX6xmf3-Fgnk5pF4mbjltDQ-9M6zuC0Bncmy4b5Y40IRUZnEdUKIxfz1MMC6tiwJBaArWza_Bln_drwu9zlRwGgyLzKDLUm9WR9v_Zc_MFFomTzKA6iU7NafcaE64qxWeIp6moPfkB6fYjeRMgjZqldE7ALt0DB8gNWXfrd3m-n5pObO5pkvfmIEIadxKcqegUW_Y3eiIG1h6xLSq6IVSI_MvXH_APfV7JdJ9PT-DUgI5N0fxuAfE67rJhL7eP9nVI6oqDAozjhD5RM0l3CBmpkLH-zU7MUfhug0WGwlj3zrAdRxDWbMvthQwug0KLrn5jtNsCRNK-ugteAnMypA1Zl-uJigmMtVgyN-BLrX_Cij04hmH8vZ9G2Uoo4OBPNEeuodauR5ZZG0wDbYLgEgAY9xgRemmIo6FwY_tK042PzZgE2AZEb8ERgZjLLTCjClWm-W6ellyaJBuVncCRCkR6RyvRHLZsL1beF8fqAJkKz9zFb51cz8D-yvZN-myGiIOFDGElIua_YgDsfwUxDnZaunf3VH0QiwuNhqB4qD3_m7hiSWhX4nHUQxPkBimzlKtS0yJn0LiQp2OZpFumrGEZOunKfGJ8oh_9UNlpUksH7yDf-HBcfJHsWj3JVXy8u5tS-pp7o3nB_SWVRMSWLnI1JhaqgVW8SjKUc.QeetnOpKxmx1mjLtAmW4UQ";
-      var resourceClient = new HttpClient() { BaseAddress = new Uri("https://sandbox-quickbooks.api.intuit.com") };
       resourceClient.DefaultRequestHeaders.Authorization =
-        new AuthenticationHeaderValue("Bearer", accessToken);
-      resourceClient.DefaultRequestHeaders.Accept.Clear();
-      resourceClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+        new AuthenticationHeaderValue("Bearer", token.AccessToken);
+
       var getResourceResponse = await resourceClient.GetAsync("v3/company/193514651873324/companyinfo/193514651873324");
 
       Console.WriteLine(await getResourceResponse.Content.ReadAsStringAsync());
